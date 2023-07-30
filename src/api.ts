e666509867b215db0308a771d7d754cea3daef8d
lasithkoswattagamage <lasithvindu1@gmail.com>
2023-07-30 22:45:11 +1000

    (feat): Add dispatch_workflow method

---
+++ src/api.ts
@@ -0,0 +1,89 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import type {GitHub} from '@actions/github/lib/utils'
+import {ActionConfig, getConfig} from './action'
+type Octokit = InstanceType<typeof GitHub>
+
+let config: ActionConfig
+let octokit: Octokit
+
+export function init(cfg?: ActionConfig): void {
+  config = cfg || getConfig()
+  octokit = github.getOctokit(config.token)
+}
+
+export async function dispatchWorkflow(distinctId: string): Promise<void> {
+  try {
+    // https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
+    const response = await octokit.rest.actions.createWorkflowDispatch({
+      owner: config.owner,
+      repo: config.repo,
+      workflow_id: config.workflow,
+      ref: config.ref || 'main',
+      inputs: {
+        ...(config.workflowInputs ? config.workflowInputs : undefined),
+        distinct_id: distinctId
+      }
+    })
+
+    if (response.status !== 204) {
+      throw new Error(
+        `Failed to dispatch action, expected 204 but received ${response.status}`
+      )
+    }
+
+    core.info(`
+Successfully dispatched workflow:
+Repository: ${config.owner}/${config.repo}
+Branch: ${config.ref}
+Workflow ID: ${config.workflow}
+Distinct ID: ${distinctId}
+${
+  config.workflowInputs
+    ? `Workflow Inputs: ${JSON.stringify(config.workflowInputs)}`
+    : ``
+}`)
+  } catch (error) {
+    if (error instanceof Error) {
+      core.error(
+        `dispatchWorkflow: An unexpected error has occurred: ${error.message}`
+      )
+      error.stack && core.debug(error.stack)
+    }
+    throw error
+  }
+}
+
+export async function getWorkflowId(workflowFilename: string): Promise<number> {
+  try {
+    // https://docs.github.com/en/rest/reference/actions#list-repository-workflows
+    const response = await octokit.rest.actions.listRepoWorkflows({
+      owner: config.owner,
+      repo: config.repo
+    })
+
+    if (response.status !== 200) {
+      throw new Error(
+        `Failed to get workflows, expected 200 but received ${response.status}`
+      )
+    }
+
+    const workflowId = response.data.workflows.find(workflow =>
+      new RegExp(workflowFilename).test(workflow.path)
+    )?.id
+
+    if (workflowId === undefined) {
+      throw new Error(`Unable to find ID for Workflow: ${workflowFilename}`)
+    }
+
+    return workflowId
+  } catch (error) {
+    if (error instanceof Error) {
+      core.error(
+        `getWorkflowId: An unexpected error has occurred: ${error.message}`
+      )
+      error.stack && core.debug(error.stack)
+    }
+    throw error
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import * as core from '@actions/core'
+import {v4 as uuid} from 'uuid'
 import {getConfig} from './action'
+import * as api from './api'
+
+const DISTINCT_ID = uuid()
 
 async function run(): Promise<void> {
   try {
     const config = getConfig()
-    core.info(JSON.stringify(config))
+    api.init(config)
+
+    let workflowId: number
+    // Get the workflow ID if give a string
+    if (typeof config.workflow === 'string') {
+      core.info(`Fetching Workflow ID for ${config.workflow}...`)
+      workflowId = await api.getWorkflowId(config.workflow)
+      core.info(`Fetched Workflow ID: ${workflowId}`)
+    } else {
+      workflowId = config.workflow
+    }
+
+    // Dispatch the action
+    await api.dispatchWorkflow(DISTINCT_ID)
   } catch (error) {
     if (error instanceof Error) {
       core.error(`Failed to complete: ${error.message}`)
